cea9c8243e4011da65968982312eb7005b377877
Dr. Vivek Kumar <vivekneoaiims@gmail.com>
2026-02-24 19:20:57 +0530

    All icons changed to NeoNEST nest icon

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icon_111.png', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png', 'shot-about.png', 'shot-tpn.png', 'shot-gir.png', 'logo-light.png', 'logo-dark.png', 'logo-light-c.png', 'logo-dark-c.png', 'icon-tpn.png', 'icon-gir.png', 'icon-nut.png'],
+      includeAssets: ['icon_111.png', 'shot-about.png', 'shot-tpn.png', 'shot-gir.png', 'logo-light.png', 'logo-dark.png', 'logo-light-c.png', 'logo-dark-c.png', 'icon-tpn.png', 'icon-gir.png', 'icon-nut.png'],
       manifest: {
         name: 'NeoNEST - Neonatal Essential Support Tools',
         short_name: 'NeoNEST',
@@ -20,9 +20,9 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+          { src: 'icon_111.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon_111.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icon_111.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ],
         // ADD THE SCREENSHOTS ARRAY HERE
         screenshots: [
